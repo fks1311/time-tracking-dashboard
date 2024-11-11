08e4e3774e52d64fdd6028bb8f8a246155e329d5
ranyoung <fks1311@gmail.com>
2024-11-11 15:27:56 +0900

    폰트 및 스타일 지정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-function App() {
-  return <div></div>;
+import styled from "styled-components";
+import { ProfileCard } from "./components/ProfileCard";
+import { ActiveCard } from "./components/ActiveCard";
+
+export default function App() {
+  return (
+    <Layout>
+      <ProfileCard />
+    </Layout>
+  );
 }
 
-export default App;
+const Layout = styled.div`
+  width: ${window.innerWidth}px;
+  height: ${window.innerHeight}px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme: { netural_color } }) => netural_color.very_dark_blue};
+`;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Reset } from "styled-reset";
-import { ThemeProvider } from "styled-components";
+import reset from "styled-reset";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 import { theme } from "./utils/style";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const GlobalStyle = createGlobalStyle`
+  ${reset};
+  body {
+  font-family: Rubik-Light
+  }
+`;
+
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Reset />
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   </React.StrictMode>
